09ec221be744101ca36bb62f40248583e786a632
macstudent <chahal.rajdeep03@gmail.com>
2019-09-25 19:33:07 -0400

    reverse string

--- src/main/java/com/lambton/LambtonStringTools.java
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -1,4 +1,36 @@
 package com.lambton;
 
 public class LambtonStringTools {
+
+    //// question 1:
+
+    public String reverse(String string) {
+        StringBuilder rev = new StringBuilder();
+        for (int i = string.length() - 1; i >= 0; i--) {
+            rev.append(string.charAt(i));
+        }
+        return rev.toString();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/lambton/MidTermTest.java
@@ -0,0 +1,11 @@
+package com.lambton;
+
+public class MidTermTest {
+
+    public static void main(String[] args)
+    {
+        LambtonStringTools object = new LambtonStringTools();
+        System.out.println(" Lambton "+object.reverse("Lambton"));
+        System.out.println();
+    }
+}
